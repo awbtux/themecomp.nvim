b7d4ac0ec0ec4a42fd0329ee50a492ca08f7b082
awbtux <andy@antareslinux.org>
2024-02-10 13:57:14 -0800

    test config parsing

--- lua/themecomp/init.lua
+++ lua/themecomp/init.lua
@@ -1,1 +1,13 @@
--- test
+local M = {}
+
+M.config = {
+    test = "test",
+}
+
+M.setup = function(overrides)
+    for k, v in pairs(overrides or {}) do
+        M.config[k] = v
+    end
+end
+
+print(M.config.test)
