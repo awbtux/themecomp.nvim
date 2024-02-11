d67560cd6347993790305fd8be837b6a38fc584b
awbtux <andy@antareslinux.org>
2024-02-10 18:35:24 -0800

    test

--- lua/themecomp/init.lua
+++ lua/themecomp/init.lua
@@ -5,13 +5,25 @@ M.colorconv = require "themecomp.colorconv"
 
 -- default settings
 M.settings = {
-    -- directory to compile color schemes to
+    -- directory to compile color schemes to (absolute path)
     ---@type string
     colors_dir = vim.fn.stdpath("config") .. "colors",
 
     -- whether or not to use transparency
     ---@type boolean
     transparency = false,
+
+    -- which theme to use
+    ---@type string
+    theme = "onedark",
+
+    -- where to find color palettes (lua module path)
+    ---@type string
+    palette_path = "themeconp.palettes",
+
+    -- where to find the user's color palettes (lua module path)
+    ---@type string
+    user_palette_path = nil,
 }
 
 -- configure settings
@@ -27,6 +39,7 @@ M.compile = function()
     if not vim.loop.fs_stat(M.settings.colors_dir) then
         vim.fn.mkdir(M.settings.colors_dir, "p")
     end
+    print("compiled")
 end
 
 -- set up the plugin
@@ -34,18 +47,8 @@ M.setup = function(overrides)
     configure(overrides)
 
     if not vim.loop.fs_stat(M.settings.colors_dir .. "/.themes_compiled") then
-        print("test")
         M.compile()
     end
-
-    --if M.settings.transparency then
-    --    local glassy = require "base46.glassy"
-    --    for key, value in pairs(glassy) do
-    --        if highlights[key] then
-    --            highlights[key] = M.merge_tb(highlights[key], value)
-    --        end
-    --    end
-    --end
 end
 
 return M
