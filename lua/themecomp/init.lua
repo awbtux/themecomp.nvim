0e39758c9028476426c89979677cfd813e3f313d
awbtux <andy@antareslinux.org>
2024-02-10 18:51:58 -0800

    function to read theme tables

--- lua/themecomp/init.lua
+++ lua/themecomp/init.lua
@@ -7,7 +7,7 @@ M.colorconv = require "themecomp.colorconv"
 M.settings = {
     -- directory to compile color schemes to (absolute path)
     ---@type string
-    colors_dir = vim.fn.stdpath("config") .. "colors",
+    colors_dir = vim.fn.stdpath("config") .. "/colors",
 
     -- whether or not to use transparency
     ---@type boolean
@@ -18,10 +18,12 @@ M.settings = {
     theme = "onedark",
 
     -- where to find color palettes (lua module path)
+    -- overridden 
     ---@type string
-    palette_path = "themeconp.palettes",
+    palette_path = "themecomp.palettes",
 
     -- where to find the user's color palettes (lua module path)
+    -- takes precedence over palette_path
     ---@type string
     user_palette_path = nil,
 }
@@ -40,6 +42,27 @@ M.compile = function()
         vim.fn.mkdir(M.settings.colors_dir, "p")
     end
     print("compiled")
+end
+
+-- get a theme table
+M.get_theme_table = function(name)
+    -- use the default palette path
+    local theme_path = M.settings.palette_path .. "." .. name
+
+    -- use a custom 
+    if M.settings.user_palette_path then
+        local theme_path = M.settings.user_palette_path .. "." .. name
+    end
+
+    -- try to read the theme file in a protected call
+    local present, theme = pcall(require, theme_path)
+
+    -- return it if possible, complain if not
+    if present then
+        return theme
+    else
+        error "Failed to read theme table!"
+    end
 end
 
 -- set up the plugin
