0809201be1cde92e653f0a5f538c0eaad95662d7
awbtux <andy@antareslinux.org>
2024-02-10 18:06:14 -0800

    test

--- lua/themecomp/init.lua
+++ lua/themecomp/init.lua
@@ -1,28 +1,51 @@
 local M = {}
 
--- default settings
-local settings = {
-    -- absolute path to the directory compiled schemes should be stored in
-    ---@type string
-    colors_dir = vim.fn.stdpath("config") .. "/colors",
+-- functions for color-related conversions/operations
+M.colorconv = require "themecomp.colorconv"
 
-    -- integrations dir (lua module path)
+-- default settings
+M.settings = {
+    -- directory to compile color schemes to
     ---@type string
-    integrations_module = "themecomp.integrations",
+    colors_dir = vim.fn.stdpath("config") .. "colors",
+
+    -- whether or not to use transparency
+    ---@type boolean
+    transparency = false,
 }
 
 -- configure settings
 local configure = function(overrides)
     for k, v in pairs(overrides or {}) do
-        settings[k] = v
+        M.settings[k] = v
     end
-    return settings
+    return M.settings
 end
 
+-- compile themes
+M.compile = function()
+    if not vim.loop.fs_stat(M.settings.colors_dir) then
+        vim.fn.mkdir(M.settings.colors_dir, "p")
+    end
+end
 
 -- set up the plugin
 M.setup = function(overrides)
     configure(overrides)
+
+    if not vim.loop.fs_stat(M.settings.colors_dir .. "/.themes_compiled") then
+        print("test")
+        M.compile()
+    end
+
+    --if M.settings.transparency then
+    --    local glassy = require "base46.glassy"
+    --    for key, value in pairs(glassy) do
+    --        if highlights[key] then
+    --            highlights[key] = M.merge_tb(highlights[key], value)
+    --        end
+    --    end
+    --end
 end
 
 return M
