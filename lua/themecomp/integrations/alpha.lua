cb231c87e2e9b8798722ba8983a984c6683670a6
awbtux <andy@antareslinux.org>
2024-02-11 17:56:12 -0800

    color integration updates

--- lua/themecomp/integrations/alpha.lua
+++ lua/themecomp/integrations/alpha.lua
@@ -2,8 +2,8 @@ local M = {}
 
 function M.set(col16, col30)
     return {
-        AlphaHeader =   { fg = col30.grey_fg },
-        AlphaButtons =  { fg = col30.light_grey },
+        AlphaHeader =   { fg = col30.grey_fg, ctermfg = 8 },
+        AlphaButtons =  { fg = col30.light_grey, ctermfg = 8 },
     }
 end
 
