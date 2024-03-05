ccd11ccd956fe700dd1c55d55f6de11a4b66399c
awbtux <andy@antareslinux.org>
2024-03-04 18:43:17 -0800

    a

--- lua/themecomp/integrations/hardline.lua
+++ lua/themecomp/integrations/hardline.lua
@@ -1,17 +1,15 @@
 local M = {}
 
-local lighten = require("themecomp.colorconv").lighten_hex
-
 function M.set(scheme, opts)
     local col16 = scheme.base16
     local col30 = scheme.base30
 
     return {
-        Hardline_bufferline_background = { fg = lighten(col30.green, -10), bg = lighten(col30.statusline_bg, scheme.type == "light" and -3 or 1), ctermfg = 2, ctermbg = 8 },
-        Hardline_bufferline_background_modified = { fg = lighten(col30.blue, -10), bg = lighten(col30.statusline_bg, scheme.type == "light" and -3 or 1), ctermfg = 4, ctermbg = 8 },
-        Hardline_bufferline_current = { fg = col30.green, bg = col30.black, ctermfg = 10, ctermbg = 0 },
-        Hardline_bufferline_current_modified = { fg = col30.blue, bg = col30.black, ctermfg = 12, ctermbg = 0 },
-        Hardline_bufferline_separator = { bg = lighten(col30.statusline_bg, scheme.type == "light" and -3 or 1), ctermbg = 8 },
+        Hardline_bufferline_background = {},
+        Hardline_bufferline_background_modified = {},
+        Hardline_bufferline_current = {},
+        Hardline_bufferline_current_modified = {},
+        Hardline_bufferline_separator = {},
         Hardline_error_active = {},
         Hardline_error_inactive = {},
         Hardline_high_active = {},
