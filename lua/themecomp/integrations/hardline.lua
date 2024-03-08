c94388abf8cf3431b48853555337d363d3675991
awbtux <andy@antareslinux.org>
2024-03-07 21:54:41 -0800

    undo update

--- lua/themecomp/integrations/hardline.lua
+++ lua/themecomp/integrations/hardline.lua
@@ -1,17 +1,15 @@
 local M = {}
 
-local lighten = require("themecomp.colorconv").lighten_hex
-
 function M.set(scheme, opts)
     local col16 = scheme.base16
     local col30 = scheme.base30
 
     return {
-        Hardline_bufferline_background = { fg = lighten(col30.green, vim.o.bg == "light" and 10 or -20), bg = "none", ctermfg = 2, ctermbg = "none" },
-        Hardline_bufferline_background_modified = { fg = lighten(col30.blue, vim.o.bg == "light" and 10 or -20), bg = "none", ctermfg = 4, ctermbg = "none" },
-        Hardline_bufferline_current = { fg = col30.green, bg = "none", ctermfg = 10, ctermbg = "none", bold = true },
-        Hardline_bufferline_current_modified = { fg = col30.blue, bg = "none", ctermfg = 12, ctermbg = "none", bold = true },
-        Hardline_bufferline_separator = { link = "TabLine" },
+        Hardline_bufferline_background = {},
+        Hardline_bufferline_background_modified = {},
+        Hardline_bufferline_current = {},
+        Hardline_bufferline_current_modified = {},
+        Hardline_bufferline_separator = {},
         Hardline_error_active = {},
         Hardline_error_inactive = {},
         Hardline_high_active = {},
